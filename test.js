5488d5dd3ee3fddfc21352de131315dfbfb516b2
long.nguyen <long.nguyen@icomm.vn>
2026-09-06 20:25:37 +0700

    JSON.stringify(route)

--- test.js
+++ test.js
@@ -1,9 +1,17 @@
 (() => {
   log("Test simulation started");
-  locsim_test();
+  (() => {
+    const route = [
+      [10.776889, 106.700806],
+      [10.77705, 106.701],
+      [10.77725, 106.7012],
+      [10.77745, 106.7014],
+      [10.77765, 106.7016],
+      [10.77785, 106.7018],
+    ];
 
-  locsim_start(10.776889, 106.700806);
-  setTimeout(() => {
-    locsim_stop();
-  }, 10000);
+    const ok = locsim_start_route(JSON.stringify(route));
+
+    log("locsim_start_route = " + ok);
+  })();
 })();
